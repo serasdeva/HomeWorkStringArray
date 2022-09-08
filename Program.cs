1583af3d0b10a3228fb1679acc675f027a2a32b8
serasdeva <serasdeva@gmail.com>
2022-09-08 21:40:00 +0700

    Код на C# - реализация

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] list = new String[] { "hello", "2", "world", ":-)" };
+string[] list2 = new String[] { };
+int len = 0;
+
+int listLength = list.Length;
+for (int i = 0; i < listLength; i++)
+{
+    if (list[i].Length <= 3)
+    {
+        Array.Resize(ref list2, list2.Length + 1);
+        len = list2.Length;
+        list2[len - 1] = list[i];
+    }
+}
+
+
+int list2Length = list2.Length;
+for (int j = 0; j < list2Length; j++)
+{
+    Console.WriteLine("   [{0}] : {1}", j, list2[j]);
+}
